dfebadb0db334e5c0fa4b00b5becea678fff83ab
Peter Weber <peterweber1406@googlemail.com>
2020-09-16 19:01:15 +0200

    Optimize+bechreibung

--- PrimCal/Main.cpp
+++ PrimCal/Main.cpp
@@ -1,6 +1,5 @@
 #include "Header.h"
 #include <iostream>
-#include <iostream>
 #include <fstream>
 
 using namespace std;
@@ -10,43 +9,39 @@ int main()
     cout << "Wählen sie nun den bereich in dem sie nach Primzahlen suchen wollen\n";
 	cout << "Von\n"; int start; cin >> start;
 	cout << "Bis\n"; int end; cin >> end;
-    //Eingabe check
+    //Eingabe der start und end werte
 	if (start > end)
 	{
 		cout << "Der angegebender Bereich ist fehlerhaft!!!";
 		return 0;
 	}
 
-	ofstream data;
-	data.open("Primzahlen.txt", ios::out | ios::in);
-
-	int counterlane = 0;
+	//inizailisirung eines zähler für die primzahlen
 	int countprim = 0;
-	float Ver;
+
 	for (int i = start;i < end;i++)
 	{
+		//funktion zu überprüfung einerzal true wenn pimezahl
 		if (Prim(i) == true)
 		{
 			countprim ++;
 			cout << i;
-			counterlane++;
-			if (counterlane == 10)
+
+			//Zeilen umbruch alle x primzahlen
+			if ((countprim%10) == 0)
 			{
 				cout << " \n";
-				counterlane = 0;
 			}
 			else cout << ",";
 		}
 	}
-	Ver = float(countprim) / float(end - start);
+	//verhältnis von zahlen und primzahlen
+	float Ver = float(countprim) / float(end - start);
 
-	cout << "\n In :";
+	cout << "\n Von :";
 	cout << end-start;
-	cout << " Soviele Prim gefunden :";
+	cout << " Zahlen sind :";
 	cout << countprim;
-	cout << " Verhätnis :";
+	cout << " Primzahlen das Verhätnis ist :";
 	cout << Ver;
-	
-	data.close();
-	return 0;
 }
--- PrimCal/Prim.cpp
+++ PrimCal/Prim.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
 bool Prim(int Numb)
-{
+{	
+	//1 und 0 werden von anfang an ausgeschlossen
+	if (Numb == 0 || Numb == 1) { return false; }
+	//göster teiler kann nur sqrt(Numb) sein.
 	for (int i = 2;i <= sqrt(Numb); i+=2)
 	{
+		//durch zeir wird gepüft danach auf ungrade zahlen getestet
 		if (i == 4)i = 3;
 
+		//Number wird auf den teiler i getestet
 		if ((Numb%i) == 0)
-		{
-			return false;
-		}
-		else if (Numb == 0 || Numb == 1)
 		{
 			return false;
 		}
